eb2824a307a01708cbf62deedee094917b7706de
tiulpin <viktor@tiulp.in>
2026-06-04 21:31:59 +0200

    feat(api): add GetBuildTypeSettings and GetBuildTypeSetting methods

---
+++ api/settings.go
@@ -0,0 +1,53 @@
+package api
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// SettingsList represents a list of build configuration settings
+type SettingsList struct {
+	Count    int       `json:"count"`
+	Property []Setting `json:"property"`
+}
+
+// Setting represents a single build configuration setting
+type Setting struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+// GetBuildTypeSettings returns all settings for a build configuration
+func (c *Client) GetBuildTypeSettings(buildTypeID string) (*SettingsList, error) {
+	path := fmt.Sprintf("/app/rest/buildTypes/id:%s/settings", buildTypeID)
+
+	var result SettingsList
+	if err := c.get(c.ctx(), path, &result); err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
+
+// GetBuildTypeSetting returns the raw plain-text value of a single build configuration setting
+func (c *Client) GetBuildTypeSetting(buildTypeID, name string) (string, error) {
+	path := fmt.Sprintf("/app/rest/buildTypes/id:%s/settings/%s", buildTypeID, name)
+
+	resp, err := c.doRequestWithAccept(c.ctx(), "GET", path, nil, "text/plain")
+	if err != nil {
+		return "", err
+	}
+	defer func() { _ = resp.Body.Close() }()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", c.handleErrorResponse(resp)
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
+}
